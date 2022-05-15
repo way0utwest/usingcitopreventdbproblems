48e4f8b76a87d4f7de6c5708f6037fc9eb4300af
steve jones way0utwest <way0utwest@hotmail.com>
2022-05-15 18:32:38 -0500

    added more demo code

--- 4_tsqlt_SQLInjection.sql
+++ 4_tsqlt_SQLInjection.sql
@@ -1,9 +1,14 @@
-CREATE PROCEDURE SearchArticles
+/*
+USING CI AND TESTING TO PREVENT DATABASE PROBLEMS
+
+Create a procedure with a security issue. This is a SQL Injection problem (potentially)
+*/
+CREATE OR ALTER PROCEDURE SearchArticles
 	@SearchTerm VARCHAR(100)
 AS
 BEGIN
   DECLARE @sql VARCHAR(MAX) = 'SELECT ArticlesID FROM dbo.Articles WHERE Article LIKE ''%' + @SearchTerm + '%'''
-  EXEC @sql
+  EXEC(@sql)
 RETURN
 END
 GO
